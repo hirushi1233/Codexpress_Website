ac2058fc80a780bdac6cbf2a7d815bab96db7b74
Sanduni Hirushika Rathnayake <sandunihirushikarathnayake@gmail.com>
2025-12-09 11:24:40 +0530

    8 th commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
+use App\Http\Controllers\TechnologyController;
+
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/technology', function () {
@@ -17,3 +19,10 @@ Route::get('/careers', function () {
 Route::get('/courses', function () {
     return view('courses');
 })->name('courses');
+
+
+
+
+
+// Replace your existing /technology route with this:
+Route::get('/technology', [TechnologyController::class, 'index'])->name('technology.index');
